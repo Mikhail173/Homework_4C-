3ee8a3b9c27e137757e0aeac4c1544415de0108c
Mikhail173 <2016.Nevermind.2020@gmail.com>
2022-11-08 00:57:38 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// То ли задача поставлена не правильно, то ли примеры не от нее.
+// Я сделал, чтобы вводился размер массива, заполнялся случайными
+// числами, максимальное из которых тоже вводилось, и массив выводился в консоль.
+Console.WriteLine("Input the size of the array");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max number");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = GetArray(size);
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i=0; i<size; i++)
+    {
+        array[i]=new Random().Next(0,max+1);
+    }
+    return array;
+}
+Console.WriteLine(String.Join(",",array));
